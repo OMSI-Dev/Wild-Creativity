7f927b93daa97b33b04fe7b446ee95dcba6571b5
Landtree <delandtree@gmail.com>
2023-07-21 09:06:17 -0700

    demo mode

--- Bird_Beaks/uController/src/main.cpp
+++ Bird_Beaks/uController/src/main.cpp
@@ -14,16 +14,17 @@ void setup()
 
 audioOut.begin();
 nutBtn.attach(nutPin, INPUT_PULLUP);
-nutBtn.interval(5);
+nutBtn.interval(1);
 nutBtn.setPressedState(LOW);
 
 flowerBtn.attach(flowerPin, INPUT_PULLUP);
-flowerBtn.interval(5);
+flowerBtn.interval(1);
 flowerBtn.setPressedState(LOW);
 
 bugBtn.attach(bugPin, INPUT_PULLUP);
-bugBtn.interval(5);
+bugBtn.interval(1);
 bugBtn.setPressedState(LOW);
+
 FastLED.addLeds<NEOPIXEL,nutLight>(Nseq, Nut_led);
 FastLED.addLeds<NEOPIXEL,flowerLight>(Fseq, flower_led);
 FastLED.addLeds<NEOPIXEL,bugLight>(Bseq, bug_led);
@@ -31,7 +32,7 @@ FastLED.addLeds<NEOPIXEL,Treecreeper_Light>(Treeseq, puck_led);
 FastLED.addLeds<NEOPIXEL,Hummingbird_Light>(Humseq, puck_led);
 FastLED.addLeds<NEOPIXEL,Finch_Light>(Finseq, puck_led);
 
-//FastLED.setMaxPowerInVoltsAndMilliamps(5,17000);
+FastLED.setMaxPowerInVoltsAndMilliamps(5,17000);
 FastLED.show();
 
 //starts all timers they will reset themselves when they are first used
@@ -47,6 +48,21 @@ Serial.begin(9600);
 #ifdef debug 
 Serial.println("exiting setup");
 #endif
+
+  Nseq.fill_solid(teal);
+  Finseq.fill_solid(teal);
+  FastLED.show();
+   delay(1000);
+  Nseq.fill_solid(CRGB::Black);
+  Finseq.fill_solid(CRGB::Black);
+  FastLED.show();
+      Bseq.fill_solid(red);
+    Treeseq.fill_solid(red);
+    FastLED.show();
+    delay(1000);
+        Bseq.fill_solid(CRGB::Black);
+    Treeseq.fill_solid(CRGB::Black);
+FastLED.show();
 }
 
 void btnCheck()
@@ -59,225 +75,64 @@ void btnCheck()
   nutState = nutBtn.isPressed();
   flowerState = flowerBtn.isPressed();
 
+  Serial.print("NUt: ");
+  Serial.println(nutBtn.isPressed());
 }
 
 void loop() 
 {
-  /*
-  //check to see if the attractor should run
-  if(gameState == false && playOnce == true)
-  {
-      //set track 1 to loop
-      audioOut.loop(1,1);
-      audioOut.playTrack(1);
-      playOnce = false;
-  }
- //update button states
-
-  btnCheck();
-  
-  if(nutState == true)
-  {
-    if(gameState == false)
-    {
-      //update to start game
-      gameState = true;
-      audioOut.stopTrack(1);
-      gameTimer.setTime(gameTime);
-    }else if(nutTriggered == false)
-    { 
-        //update buttons triggered status
-        nutTriggered = true;
-        //update points
-        points = points + 1;
-        audioOut.playTrack(2);
-        Nseq[Nut_led]= CRGB::Teal;
-        Finseq[puck_led]= CRGB:: Teal;
-        FastLED.show();
-        #ifdef debug
-        Serial.println("Nut button Pressed & registered");
-        Serial.print("Current Points: ");
-        Serial.println(points);
-        Serial.print("Nut button trigged status: ");
-        Serial.println(nutTriggered);
-        #endif                
-    }     
-  }
-
-  //Checks if the flower button has been pressed
-  //Checks if the game has already started
-  //updates points and triggered status
-  if(flowerState == true)
-  {
-    if(gameState == false)
-    {
-      //update to start game
-      gameState = true;
-      audioOut.stopTrack(1);
-      gameTimer.setTime(gameTime);
-    }else if(flowerTriggered == false)
-    { 
-        //update buttons triggered status
-        flowerTriggered = true;
-        //update points
-        points = points + 1;
-        audioOut.playTrack(3);
-        Fseq[flower_led]= CRGB::Orange;
-        Humseq[puck_led]= CRGB:: Orange;
-        FastLED.show();
-        #ifdef debug
-        Serial.println("Flower button Pressed & registered");
-        Serial.print("Current Points: ");
-        Serial.println(points);
-        Serial.print("Flower button trigged status: ");
-        Serial.println(flowerTriggered);
-        #endif    
-    }
-
-  }
-
-  //Checks if the bug button has been pressed
-  //Checks if the game has already started
-  //updates points and triggered status
-  if(bugState == true)
-  {
-    if(gameState == false)
-    {
-      //update to start game
-      gameState = true;
-      audioOut.stopTrack(1);
-      gameTimer.setTime(gameTime);
-    }else if(bugTriggered == false)
-    { 
-        //update buttons triggered status
-        bugTriggered = true;
-        //update points
-        points = points + 1;
-        audioOut.playTrack(4);
-        Bseq[bug_led]= CRGB::Red;
-        Treeseq[bug_led]= CRGB::Red;
-        FastLED.show();
-        #ifdef debug
-        Serial.println("Bug button Pressed & registered");
-        Serial.print("Current Points: ");
-        Serial.println(points);
-        Serial.print("Bug button trigged status: ");
-        Serial.println(bugTriggered);
-        #endif   
-    } 
  
 
-  }
+  /* TEMP CODE
+  !!!!!!!!!!!!!!!!!!!Delete after implamenting final code!!!!!!!!!!!!!!!!!!!!!
 
-  //Checks triggered status and updates lights accordingly if the game is running
-  if(gameState == true && winPlayOnce == true )
+  */
+
+  btnCheck();
+
+  if(bugState)
   {
-    if(bugTriggered == true){digitalWrite(bugLight, HIGH);}else(digitalWrite(bugLight, LOW));
-    if(nutTriggered == true){digitalWrite(nutLight, HIGH);}else(digitalWrite(nutLight, LOW));
-    if(flowerTriggered == true){digitalWrite(flowerLight, HIGH);}else(digitalWrite(flowerLight, LOW));
+    Bseq.fill_solid(red);
+    Treeseq.fill_solid(red);\
+    digitalWrite(13,HIGH);
   }
 
-  //if they earn enough points by the game time
-  // win sequence starts and resets
-  if(points == pointMax)
+  if (nutState)
   {
-    
-    if(winAudioTimerFlag == true)
-    {
-      //restart the game timer rather than stopping or it trips an early restart
-      //
-      gameTimer.restart();
-      winAudioTimer.setTime(1500);
-      winAudioTimerFlag = false;
-      #ifdef debug
-      Serial.println(" ");      
-      Serial.println("Game won!");
-      Serial.print("winAudioTimerFlag: ");
-      Serial.println(winAudioTimerFlag);
-      Serial.print("winAudioTimer running: ");
-      Serial.println(winAudioTimer.running());
-      #endif       
-    }
-
-    #ifdef debug
-    Serial.println(" ");
-    Serial.println("Start win Sequence!");
-    Serial.print("stopOnce: ");
-    Serial.println(stopOnce);
-    Serial.print("winPlayOnce: ");
-    Serial.println(winPlayOnce);
-    Serial.print("winAudioTimer running: ");
-    Serial.println(winAudioTimer.getTime());
-    Serial.println(" ");
-    #endif 
-
-    //play win sound
-    if(winAudioTimer.running() == false)
-    {
-        #ifdef debug
-        Serial.println(" ");
-        Serial.print("resetFlag: ");
-        Serial.println(resetFlag);    
-        Serial.print("winAudioTimer running: ");
-        Serial.println(winAudioTimer.running());
-        Serial.println(" ");
-        #endif
-      if(stopOnce == true)
-      {
-      audioOut.stopAllTracks(); 
-      stopOnce = false;  
-      }
-
-      if(winPlayOnce == true)
-      {
-      audioOut.playTrack(5);
-      winPlayOnce = false;
-      resetTimer.setTime(resetTime);
-      resetFlag = true;         
-      }
-
-    }
-
-      //win sequence
-      if(winPlayOnce == false)
-      {
-      lightWin();
-      }
-
-      //reset game after
-      if (resetTimer.running() == false && resetFlag == true)
-      {
-        resetGame();
-      }
-
-
-
+  Nseq.fill_solid(teal);
+  Finseq.fill_solid(teal);
   }
 
-
-  if(gameState == false)
+  if (flowerState)
   {
-    #ifdef debug
-    Serial.println(" going to attract");
-    #endif
-
-    lightAttract();
+    Fseq.fill_solid(orange);
+    Humseq.fill_solid(orange);
   }
 
-  if(gameTimer.running() == false && gameState == true)
+  if(!bugState)
   {
-    resetGame();
+    Bseq.fill_solid(CRGB::Black);
+    Treeseq.fill_solid(CRGB::Black);
+    digitalWrite(13,LOW);
   }
-*/
-//Serial.print("entering lightwin: ");
-lightWin();
-//Serial.println("exiting lightwin: ");
-//Bseq.fill_solid(CRGB::Red);
-Nseq.fill_solid(CRGB::Red);
-//Fseq.fill_solid(CRGB::Red);
-//Finseq.fill_solid(CRGB::Red);
-//Treeseq.fill_solid(CRGB::Red);
-//Humseq.fill_solid(CRGB::Red);
-FastLED.show();
+
+  if (!nutState)
+  {
+  Nseq.fill_solid(CRGB::Black);
+  Finseq.fill_solid(CRGB::Black);
+  }
+
+  if (!flowerState)
+  {
+    Fseq.fill_solid(CRGB::Black);
+    Humseq.fill_solid(CRGB::Black);
+  }
+
+  
+
+  FastLED.show();
+
+Serial.println(nutState);
+
 
 }
